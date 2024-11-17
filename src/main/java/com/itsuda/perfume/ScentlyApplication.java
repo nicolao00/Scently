4f6e9b623463fa2d71e2ef081483cfd9bdcf7703
nicolao00 <nicolao00@naver.com>
2024-11-17 20:09:38 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/itsuda/perfume/ScentlyApplication.java
@@ -0,0 +1,13 @@
+package com.itsuda.perfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsuda/perfume/ScentlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsuda.perfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
