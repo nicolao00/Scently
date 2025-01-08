8843124aa55e77f13a0c28cf0b1713b9e1656ab0
nicolao00 <nicolao00@naver.com>
2025-01-08 15:50:24 +0900

    fix: 향수 제조국 범위 축소

--- src/main/java/com/itsuda/perfume/domain/type/CountryType.java
+++ src/main/java/com/itsuda/perfume/domain/type/CountryType.java
@@ -5,16 +5,8 @@ public enum CountryType {
     ITALY("이탈리아"),
     USA("미국"),
     UK("영국"),
-    SWEDEN("스웨덴"),
-    SWITZERLAND("스위스"),
-    GERMANY("독일"),
-    SPAIN("스페인"),
-    NETHERLANDS("네덜란드"),
-    BELGIUM("벨기에"),
-    DENMARK("덴마크"),
-    JAPAN("일본"),
-    UAE("아랍에미리트"),
-    KOREA("한국");
+    KOREA("한국"),
+    SWEDEN("스웨덴");
     
     private final String description;
     
